697ced1e28ffa5abf692a5c60f381fbe97aaa144
Lars Thorup <lars@zealake.com>
2016-05-19 10:52:16 +0200

    0.2.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
 module.exports = function (pathRelativeToRoot) {
-  var root = '../..'; // Note: this assumes that local-require is installed from package.json in the root folder
+  var root = '../../..'; // Note: this assumes that local-require is installed from package.json in the root folder
   return require(path.join(__dirname, root, pathRelativeToRoot));
 }
